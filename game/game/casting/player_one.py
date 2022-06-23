4c0840152184d823dbf36d0034989cfad177a1a7
Bryce Sanders <104106284+bryce-sanders@users.noreply.github.com>
2022-06-23 09:02:42 -0700

    Bones of the Project

---
+++ game/game/casting/player_one.py
@@ -0,0 +1,33 @@
+import constants
+from game.casting.actor import Actor
+from game.casting.player import Player
+from game.shared.point import Point
+
+
+class PlayerOne(Player):
+    """
+    A player on a motorcycle with a long energy trail.
+    
+    The responsibility of Player is to move itself.
+
+    """
+    def __init__(self):
+        super().__init__()
+    
+    def _prepare_trail(self):
+        x = int(90)
+        y = int(150)
+
+        for i in range(constants.TRAIL_LENGTH):
+            position = Point(x - i * constants.CELL_SIZE, y)
+            velocity = Point(1 * constants.CELL_SIZE, 0)
+            text = "@" if i == 0 else "#"
+            color = constants.RED
+            
+            segment = Actor()
+            segment.set_position(position)
+            segment.set_velocity(velocity)
+            segment.set_text(text)
+            segment.set_color(color)
+            self._trail.append(segment)
+        
---
+++ game/game/scripting/draw_actors_action.py
@@ -0,0 +1,36 @@
+from game.scripting.action import Action
+
+
+class DrawActorsAction(Action):
+    """
+    An output action that draws all the actors.
+    
+    The responsibility of DrawActorsAction is to draw all the actors.
+
+    Attributes:
+        _video_service (VideoService): An instance of VideoService.
+    """
+
+    def __init__(self, video_service):
+        """Constructs a new DrawActorsAction using the specified VideoService.
+        
+        Args:
+            video_service (VideoService): An instance of VideoService.
+        """
+        self._video_service = video_service
+
+    def execute(self, cast, script):
+        """Executes the draw actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        player = cast.get_first_actor("players")
+        trail = player.get_trail()
+        messages = cast.get_actors("messages")
+
+        self._video_service.clear_buffer()
+        self._video_service.draw_actors(trail)
+        self._video_service.draw_actors(messages, True)
+        self._video_service.flush_buffer()
